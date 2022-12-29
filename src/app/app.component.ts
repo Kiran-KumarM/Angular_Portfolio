ccc2f74c1459de098327a2f6af658b1cee248cbd
Kiran-KumarM <kiran.kumar.msingh@gmail.com>
2022-12-29 15:38:47 +0530

    Added header, about, skills sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'portfolio';
+  isToggel: boolean = false;
+  skill_index: number = 1;
+
+  navToggle() {
+    this.isToggel = !this.isToggel;
+  }
+  onSkillClick(index: number) {
+    this.skill_index = index;
+  }
 }
